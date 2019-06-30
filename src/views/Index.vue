da8109c14f5094bb3f4afee3e84536e5bfb32e02
Igor Serafim <igor.serafimsantos@gmail.com>
2019-06-30 17:49:56 -0300

    novas páginas

---
+++ src/views/Index.vue
@@ -0,0 +1,40 @@
+ <template>
+    <section class="search">
+        <div class="navbar-fixed">
+        <nav class="bg-primary">
+            <div class="nav-wrapper">
+            <img class="brand-logo" src='' alt='Logo Cobertura' />
+            <ul class="right">
+                <li><a href="#">Lorem</a></li>
+                <li><a href="#">Ipsum</a></li>
+                <li><a href="#">Dolor</a></li>
+                <li><a href="/login" class="btn btn-white">Entrar</a></li>
+            </ul>
+            </div>
+        </nav>
+        </div>
+
+        <div class="big-container">
+        <div class="cta">
+            <h1 class="display">Encontre a sua nova casa e agende <br> uma visita sem compromisso.</h1>
+            <form class="input-group">
+            <input type="text" name="search-item" id="search-item" class="input-search" placeholder="Onde você quer morar?">
+            <button class="btn btn-search fa"></button>
+            </form>
+            <div class="text-center mt-3">
+            <a href="#" class="text-white">Quero anunciar um imóvel</a>
+            </div>
+        </div>
+        <div class="decoration">
+            <img src="" alt="ilustração de um sofá" width="500" class="sofa">
+            <img src="" class="bubble">
+        </div>
+        </div>
+
+    </section>
+</template>
+
+<style lang="scss">
+@import '../assets/css/libs.css';
+@import '../assets/css/style.css';
+</style>
