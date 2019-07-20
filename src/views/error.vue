3a41dc952892f7e7051cc7131df68b8ed6ddc406
Guilherme Silva <guilhermesillva.grs@gmail.com>
2019-07-20 16:02:58 -0300

    #3 - fix animações Materialize

---
+++ src/views/error.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <img src="https://cdn.pixabay.com/photo/2019/03/07/23/55/ncorrect-answer-4041502_960_720.png" align="center" alt="Logo Cobertura" />
+        <div>
+            <h1>ERROR 404</h1>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+@import '../assets/css/libs.css';
+@import '../assets/css/style.css';
+</style>
