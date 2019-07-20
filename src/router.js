e4dd2ca6a573a42cc96b8a9a49a5d93679fa9d45
Guilherme Silva <guilhermesillva.grs@gmail.com>
2019-07-20 13:53:05 -0300

    #3 - Implementando router

--- src/router.js
+++ src/router.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from './views/Login'
 import Index from './views/Index'
-import Erro from './views/Error'
-import Profile from './views/Profile'
 
 Vue.use(Router)
 
@@ -11,19 +8,8 @@ export default new Router ({
     mode: 'hash',
     routes: [{
         path: '/',
-        component: Login,
-        name: 'login'
-    }, {
-        path: '/index',
         component: Index,
         name: 'index'
-    }, {
-        path: '/profile',
-        component: Profile,
-        name: 'profile'
-    }, {
-        path: '*',
-        component: Erro,
-        name: 'error'
-    }]
+        }, 
+    ]
 })
