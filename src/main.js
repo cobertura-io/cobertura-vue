e4389ac57bb268ff3e0445d674f63365c9ccb874
Igor Serafim <igor.serafimsantos@gmail.com>
2019-07-21 13:54:34 -0300

    commit inicial, instalando e configurando vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 new Vue({
+  store,
   router,
   el: '#app',
   render: h => h(App)
